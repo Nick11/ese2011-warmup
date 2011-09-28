981f27a1b6f5c3817df08cfdc8501e0c3fdeb178
Nick11 <scheuing@students.unibe.ch>
2011-09-28 11:52:20 +0200

    final version

--- Calendar/src/core/Calendar.java
+++ Calendar/src/core/Calendar.java
@@ -2,38 +2,73 @@ package core;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class Calendar {
 
 	private String name;
 	private User user;
-	private ArrayList<Event> events;
+	private HashMap<GregorianCalendar, ArrayList<Event>> events;
 	
 	public Calendar(User user){
 		this.name = user.getName()+"'s Calendar";
 		this.user = user;
-		this.events = new ArrayList<Event>();
+		this.events = new HashMap<GregorianCalendar, ArrayList<Event>>();
 		
 	}
 	/**
-	 * 
+	 * Adds Event to events
 	 * @param event
 	 * @param user
 	 * @return new Event
 	 */
 	public Event addEvent(Event event, User user){
 		assert isOwner(user);
-		events.add(event);
+		GregorianCalendar beginningOfDay = getBeginningOfDay(event.getStartDate());
+		if(!events.containsKey(beginningOfDay) ){ //is there already another event with the same startDate?
+			ArrayList<Event> dateList = new ArrayList<Event>();
+			dateList.add(event);
+			events.put(beginningOfDay, dateList);
+		}
+		else{
+			ArrayList<Event> list = events.get(beginningOfDay);
+			for(Event eventInList: list){
+				if(!event.getStartDate().after(eventInList.getStartDate())){ //event.getStartDate() <= eventInList.getStartDate()
+					list.add(list.indexOf(eventInList), event);
+					break;
+				}
+					
+			}
+			if (!list.contains(event))
+				list.add(event);
+		}
 		return event;
 	}
-	//TODO: implementation
 	public ArrayList<Event> listOneDaysEvents(GregorianCalendar date, User user){
-		return null;
+		HashMap<GregorianCalendar, ArrayList<Event>> allEvents;
+		ArrayList<Event> daysEvents = new ArrayList<Event>();
+		date = getBeginningOfDay(date);
+		if(isOwner(user))
+			allEvents = getAllEvents(user);
+		else
+			allEvents = getPublicEvents();
+		if(allEvents.containsKey(date))
+			return allEvents.get(date);
+		else
+			return new ArrayList<Event>();
 	}
-		
-	//TODO: implementation
-	public ArrayList<Event> iterateFromDate(User user){
-		return null;
+	
+	public Iterator<Event> iterateFromDate(GregorianCalendar date, User user){
+		date = getBeginningOfDay(date);
+		ArrayList<Event> eventsFromDate = new ArrayList<Event>();
+		GregorianCalendar[] dates = events.keySet().toArray(new GregorianCalendar[events.size()]); //unsure about this
+		for(int i=0; i<dates.length; i++){
+			if(!dates[i].before(date)){
+				eventsFromDate.addAll(listOneDaysEvents(dates[i],user));
+			}
+		}
+		return eventsFromDate.iterator();
 	}
 	
 	//is'es
@@ -59,7 +94,7 @@ public class Calendar {
 	 * @param user For Identification
 	 * @return ArrayList of all Events. public and private. 
 	 */
-	public ArrayList<Event> getAllEvents(User user){
+	public HashMap<GregorianCalendar, ArrayList<Event>> getAllEvents(User user){
 		assert isOwner(user);
 		return this.events;
 	}
@@ -67,13 +102,44 @@ public class Calendar {
 	 * Public events are visible for everybody
 	 * @return Public Events
 	 */
-	public ArrayList<Event> getPublicEvents(){
-		ArrayList<Event> publicEvents = new ArrayList<Event>();
-		for(Event event: events){
-			if(!event.isPrivate())
-				publicEvents.add(event);
+	public HashMap<GregorianCalendar, ArrayList<Event>> getPublicEvents(){
+		ArrayList<Event> dateList;
+		ArrayList<Event> publicList = new ArrayList<Event>();
+		GregorianCalendar currentDate;
+		
+		HashMap<GregorianCalendar, ArrayList<Event>> publicEvents = new HashMap<GregorianCalendar, ArrayList<Event>>();
+		Iterator<ArrayList<Event>> iterator = events.values().iterator();
+		
+		while(iterator.hasNext()){
+			dateList = deepCloneArrayList(iterator.next());
+			assert dateList.size()>0; //this is a bit unsave, if the remove method is not implementet carfully. there could be an empty ArrayList
+			currentDate = getBeginningOfDay(dateList.get(0).getStartDate()); 
+			
+			for( Event event: dateList){
+				if(!event.isPrivate())
+					publicList.add(event);
+			}
+			if(publicList.size() > 0)
+				publicEvents.put(currentDate, publicList);
+			publicList = new ArrayList<Event>();
 		}
 		return publicEvents;
+	}
+	
+	//public for testing! is there a better way?
+	public ArrayList deepCloneArrayList(ArrayList originalList){
+		ArrayList newList = new ArrayList();
+		for(Object object: originalList){
+			newList.add(object);
+		}
+		return newList;
+	}
+	//public for testing! is there a better way?
+	public GregorianCalendar getBeginningOfDay(GregorianCalendar date){
+		GregorianCalendar beginningOfDay = (GregorianCalendar) date.clone();
+		beginningOfDay.set(GregorianCalendar.HOUR_OF_DAY, 0);
+		beginningOfDay.set(GregorianCalendar.MINUTE, 0);
+		return beginningOfDay;
 	}
 	/**
 	 * Changes calendar's name.
--- Calendar/src/test/CalendarTest.java
+++ Calendar/src/test/CalendarTest.java
@@ -2,8 +2,11 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +18,8 @@ public class CalendarTest {
 	private User user1; 
 	private GregorianCalendar date0 = new GregorianCalendar(2001,GregorianCalendar.JANUARY,1,10,30);
 	private GregorianCalendar date1 = new GregorianCalendar(2001,GregorianCalendar.JANUARY,2,9,00);
+	private GregorianCalendar date2 = new GregorianCalendar(2001,GregorianCalendar.JANUARY,3,11,30);
+	private GregorianCalendar date3 = new GregorianCalendar(2001,GregorianCalendar.JANUARY,4,12,00);
 	
 	@Before
 	public void before(){
@@ -40,9 +45,11 @@ public class CalendarTest {
 	}
 	
 	@Test
-	public void sholdHaveEvents(){
+	public void sholdHaveEvents(){ //TODO: only tests the number of dates with events, but not the number of events per day
 		calendar.addEvent(new Event("event0", date0, date1, true),user);
-		assertEquals(calendar.getAllEvents(user).size(), 1);
+		calendar.addEvent(new Event("event0", date1, date1, false), user);
+		assertEquals(calendar.getAllEvents(user).size(), 2);
+		
 	}
 	
 	@Test
@@ -63,14 +70,88 @@ public class CalendarTest {
 		assert(assertionErrorHappend);
 	}
 	
-	@Test
+	@Test //TODO: same as shouldHaveEvents() see above
 	public void allEventsShouldContainPublicEvents(){
 		calendar.addEvent(new Event("event0", date0, date1, true), user);
-		calendar.addEvent(new Event("event0", date0, date1, false), user);
-		assert calendar.getAllEvents(user).containsAll(calendar.getPublicEvents());
-		assertFalse( calendar.getPublicEvents().containsAll(calendar.getAllEvents(user)) );
+		calendar.addEvent(new Event("event1", date1, date2, false), user);
+		calendar.addEvent(new Event("event2", date1, date3, true), user);
+		calendar.addEvent(new Event("event3", date2, date3, false), user);
+		assert (calendar.getAllEvents(user).size() > calendar.getPublicEvents().size());
+		assertEquals (calendar.getPublicEvents().size(), 2);
+		assertEquals (calendar.getAllEvents(user).size(), 3);
 	}
 	
+	@Test
+	public void shouldCloneArrayList(){
+		ArrayList list0 = new ArrayList();
+		ArrayList list1 = new ArrayList();
+		list1.add(1);
+		list1.add(2);
+		ArrayList clone0 = calendar.deepCloneArrayList(list0);
+		assertEquals( clone0.size(), 0);
+		assert(clone0.containsAll(list0) && list0.containsAll(clone0));
+		clone0 = calendar.deepCloneArrayList(list1);
+		assertEquals( clone0.size(), 2);
+		assert(clone0.containsAll(list1) && list1.containsAll(clone0));
+		assert(clone0.containsAll(list0));
+		assertFalse(list0.containsAll(clone0));
+	}
+	
+	@Test
+	public void shouldListEventsOfOneDay(){
+		Event event0 = new Event("event0", date0, date1, false);
+		Event event1 = new Event("event1", date1, date2, true);
+		Event event2 = new Event("event2", date1, date3, false);
+		Event event3 = new Event("event3", date3, date3, true);
+		calendar.addEvent(event0, user);
+		calendar.addEvent(event1, user);
+		calendar.addEvent(event2, user);
+		calendar.addEvent(event3, user);
+		
+		ArrayList<Event> allEvents = calendar.listOneDaysEvents(date1, user);
+		assertEquals(allEvents.size(), 2);
+		assert(allEvents.contains(event1));
+		assert(allEvents.contains(event2));
+		allEvents = calendar.listOneDaysEvents(date1, user1);
+		assertEquals(allEvents.size(), 1);
+		assertFalse(allEvents.contains(event1));
+		assert(allEvents.contains(event2));
+	}
+	
+	@Test
+	public void shouldIterate(){
+		Event event0 = new Event("event0", date0, date1, false);
+		Event event1 = new Event("event1", date1, date2, true);
+		Event event2 = new Event("event2", date1, date3, false);
+		Event event3 = new Event("event3", date3, date3, true);
+		calendar.addEvent(event0, user);
+		calendar.addEvent(event1, user);
+		calendar.addEvent(event2, user);
+		calendar.addEvent(event3, user);
+		
+		Iterator<Event> iterator0 = calendar.iterateFromDate(date1, user);
+		Iterator<Event> iterator1 = calendar.iterateFromDate(date1, user1);
+		assertFalse(iterator0.equals(iterator1));
+		int counter0 = 0;
+		int counter1 = 0;
+		Event eventx;
+		while(iterator0.hasNext()){
+			eventx = iterator0.next();
+			assert ( eventx == event1 || eventx == event2 || eventx == event3 );
+			counter0++;
+		}
+		assertEquals(counter0,3);
+		while(iterator1.hasNext()){
+			assert (iterator1.next() == event2);
+			counter1++;
+		}
+		assertEquals(counter1,1);
+	}
+	
+	@Test
+	public void shouldReturnBeginningOfDay(){
+		assert( calendar.getBeginningOfDay(date2).equals(new GregorianCalendar(2001,GregorianCalendar.JANUARY,3,0,0)));
+	}
 	
 	@Test
 	public void sholdFail(){
